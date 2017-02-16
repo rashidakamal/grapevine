5138376bb33f22185ebbe00f9f8aaf7787138634
rashidakamal <rsk2161@columbia.edu>
2017-02-16 15:56:09 -0500

    edits to scraper

--- scrape/NYT_API.py
+++ scrape/NYT_API.py
@@ -15,17 +15,14 @@ def get_data(phrase, datastore=[], startyear=2016, endyear=2016):
     months = list(range(1, 13))
     start_month = "0101"
 #     end_month = "0630"
-    end_month = "1231"
+    end_month = "0131"
     
-    api_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22"+ str(phrase) + "%22&fl=web_url" +"&api-key=" + key + "&fq=source:('" + source + "')" + "&begin_date=" + str(startyear) + str(start_month) + "&end_date=" + str(endyear) + str(end_month)
-    print(api_url)
+    api_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22"+ str(phrase) + "%22" +"&api-key=" + key + "&fq=source:('" + source + "')" + "&begin_date=" + str(startyear) + str(start_month) + "&end_date=" + str(endyear) + str(end_month)
 
-#     api_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ str(phrase) + "&api-key=" + key
 
     response_prime = requests.get(api_url)    
     data_prime = response_prime.json()
 
-    print(data_prime)
     
     results_count = data_prime['response']['meta']['hits']
     print("**number of results: ", results_count)
@@ -38,7 +35,7 @@ def get_data(phrase, datastore=[], startyear=2016, endyear=2016):
 
     for p in range(0,pages):        
 
-        api_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22"+ str(phrase) + "%22&api-key=" + key + "&fq=source:('" + source + "')" + "&begin_date=" + str(startyear) + "0101" + "&end_date=" + str(endyear) + "0101" + "&page=" + str(p)
+        api_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22"+ str(phrase) + "%22&api-key=" + key + "&fq=source:('" + source + "')" + "&begin_date=" + str(startyear) + str(start_month) + "&end_date=" + str(endyear) + str(end_month) + "&page=" + str(p)
 
         content = requests.get(api_url)
         print(content.status_code)
@@ -46,9 +43,8 @@ def get_data(phrase, datastore=[], startyear=2016, endyear=2016):
         # Handle 403 bug and other errors 
         if content.status_code != 200:
             print("failed on:", api_url)
-            print(content.text)
             
-            api_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22"+ str(phrase) + "%22&fl=web_url" + "&api-key=" + key + "&fq=source:('" + source + "')" + "&begin_date=" + str(startyear) + "0101" + "&end_date=" + str(endyear) + "0101" + "&page=" + str(p)
+            api_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22"+ str(phrase) + "%22&fl=web_url" + "&api-key=" + key + "&fq=source:('" + source + "')" + "&begin_date=" + str(startyear) + str(start_month) + "&end_date=" + str(endyear) + str(end_month) + "&page=" + str(p)
             content = requests.get(api_url)
             data = content.json()
             
@@ -58,13 +54,15 @@ def get_data(phrase, datastore=[], startyear=2016, endyear=2016):
 
         if data.get("response"):
 
-       		print("data['response'] exists")
+            print("NUMBER OF DOCS:", len(data["response"]["docs"]))
 
             for d in data["response"]["docs"]:
+                
                 d["body"] = NYTPage.get_body(d['web_url'])
                 datastore.append(d)
                 print("added data point from page: ", p)
                 time.sleep(0.1)
+
 
             print("scraped page", p)
             print("__"*20)
@@ -74,7 +72,13 @@ def get_data(phrase, datastore=[], startyear=2016, endyear=2016):
             errors.append(p)
             print("Didn't see response for page: ", p)
             print("__"*20)
+
+        del data
             
         time.sleep(0.5)
         
     return errors
+
+
+
+
--- scrape/tester.py
+++ scrape/tester.py
@@ -3,13 +3,11 @@ import NYT_API
 mydata = []
 NYT_API.get_data("condition+of+anonymity", mydata)
 
+
+print("=="*20, "IN YOUR TESTER SCRIPTER", "=="*20)
+
 print(len(mydata))
-
-print("***"*10)
-
 print(mydata[0])
-
-print("==="*20)
 
 for item in mydata:
     print(item["web_url"], item["body"])
